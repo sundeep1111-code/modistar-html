8c66622f043796e8f204f249ecb1c81a2a994de3
sundeep <sundeep1111-code@gmail.com>
2020-02-02 21:36:19 +0530

    modistarhtml

---
+++ js/tab.js
@@ -0,0 +1,43 @@
+ // tabbed content
+    // http://www.entheosweb.com/tutorials/css/tabs.asp
+    $(".tab_content").hide();
+    $(".tab_content:first").show();
+
+  /* if in tab mode */
+    $("ul.tabs li").click(function() {
+		
+      $(".tab_content").hide();
+      var activeTab = $(this).attr("rel"); 
+      $("#"+activeTab).fadeIn();		
+		
+      $("ul.tabs li").removeClass("active");
+      $(this).addClass("active");
+
+	  $(".tab_drawer_heading").removeClass("d_active");
+	  $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+	  
+    /*$(".tabs").css("margin-top", function(){ 
+       return ($(".tab_container").outerHeight() - $(".tabs").outerHeight() ) / 2;
+    });*/
+    });
+    $(".tab_container").css("min-height", function(){ 
+      return $(".tabs").outerHeight() + 0;
+    });
+	/* if in drawer mode */
+	$(".tab_drawer_heading").click(function() {
+      
+      $(".tab_content").hide();
+      var d_activeTab = $(this).attr("rel"); 
+      $("#"+d_activeTab).fadeIn();
+	  
+	  $(".tab_drawer_heading").removeClass("d_active");
+      $(this).addClass("d_active");
+	  
+	  $("ul.tabs li").removeClass("active");
+	  $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+    });
+	/* Extra class "tab_last" 
+	   to add border to bottom side
+	   of last tab 
+	$('ul.tabs li').last().addClass("tab_last");*/
+	
